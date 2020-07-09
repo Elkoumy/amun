a0eba6c033910ea47d003d58a255fb01c7fce192
Gamal Elkoumy <gamal.elkomy@gmail.com>
2020-07-09 15:44:36 +0300

    initalizing guessing advantage functions

--- guessing_advantage.py
+++ guessing_advantage.py
@@ -1,3 +1,14 @@
 """
 This module will include the guessing advantage implementation.
 """
+
+def calculate_epsilon_from_delta(delta):
+    epsilon =0
+
+    return epsilon
+
+
+def calculate_epsilon_from_accuracy(accuracy):
+    epsilon=0
+
+    return epsilon
