2f2085ddd356384fc7da4972be954ee44f9866d1
Gamal Elkoumy <gamal.elkomy@gmail.com>
2020-07-21 16:46:04 +0300

    adding noise to the frequency dfg

--- differental_privacy_module.py
+++ differental_privacy_module.py
@@ -6,17 +6,38 @@ The module has two main functionalities:
     * Take accuracy as input, then calculate both epsilon and the delta (risk) correlated with it.
 """
 from guessing_advantage import calculate_epsilon_freq , calculate_epsilon_from_delta, calculate_epsilon_time, AggregateType
-def differential_privacy_with_risk( dfg_freq, dfg_time, delta, precision):
-    epsilon=1
-    accuracy=1
-    #TODO calculate epsilon
-    epsilon_freq,senstivity=calculate_epsilon_freq(dfg_freq,delta)
-    epsilon_time,senstivity=calculate_epsilon_time(dfg_time,delta,precision, aggregate_type=AggregateType.SUM)
-    #TODO adding laplace noise
+import diffprivlib.mechanisms as privacyMechanisms
+import sys
 
+
+def differential_privacy_with_risk( dfg_freq, dfg_time, delta, precision):
+    """
+    This method adds the differential privacy to the DFG of both time and frequencies.
+        * It calculates the epsilon using the guessing advantage technique.
+        * It adds laplace noise to the DFGs.
+        * It calculates the distance resulted from the noise
+    """
+    accuracy=1
+    # calculate epsilon
+    epsilon_freq,senstivity_freq=calculate_epsilon_freq(dfg_freq,delta)
+    epsilon_time,senstivity_time=calculate_epsilon_time(dfg_time,delta,precision, aggregate_type=AggregateType.SUM)
+
+    #TODO fix the sign problem in epsilon values
+    epsilon_freq=abs(epsilon_freq)
+
+
+    # adding laplace noise to DFG frequencies
+    laplace_mechanism = privacyMechanisms.LaplaceBoundedDomain()
+    laplace_mechanism.set_sensitivity(senstivity_freq).set_bounds(0, sys.maxsize).set_epsilon(epsilon_freq)
+
+    dfg_freq_new = dfg_freq
+    for key in dfg_freq_new.keys():
+        dfg_freq_new[key]= laplace_mechanism.randomise(dfg_freq_new[key])
+
+    # adding laplace noise to DFG time
     #TODO Calculate accuracy
 
-    return dfg_freq, dfg_time, epsilon_freq,epsilon_time, accuracy
+    return dfg_freq_new, dfg_time, epsilon_freq,epsilon_time, accuracy
 
 
 
