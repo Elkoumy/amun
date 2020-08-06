a68b0127ae78875655b2fc87c4ce03edf1fac9f7
Gamal Elkoumy <gamal.elkomy@gmail.com>
2020-08-06 12:44:44 +0300

    adding guessing_advantage plot

--- plot_laplace_figure.py
+++ plot_laplace_figure.py
@@ -4,7 +4,7 @@ Plotting the laplace distribution for the paper
 
 import numpy as np
 import matplotlib.pyplot as plt
-
+import scipy.stats as sp
 loc, scale = 0., 1.
 s = np.random.laplace(loc, scale, 1000)
 
@@ -34,10 +34,52 @@ x_intersection=x[ np.logical_and(x<3.0, x>-3.0)]
 y_intersection=pdf[np.logical_and(x<3.0, x>-3.0)]
 plt.fill(x_intersection,y_intersection, facecolor='blue', alpha=0.5)
 
+#hide ticks
+plt.xticks([])
+plt.yticks([])
+
+plt.xlabel("x")
+plt.ylabel("y")
 plt.savefig(r"C:\Gamal Elkoumy\PhD\OneDrive - Tartu Ülikool\Differential Privacy\paper\amun-workshop-paper\figures\laplace.pdf")
 plt.show()
 
+
+"""Plot guessing advantage"""
+#normal distribution
+loc, scale = 0., 2.4
 g = (1/(scale * np.sqrt(2 * np.pi)) *
      np.exp(-(x - loc)**2 / (2 * scale**2)))
-# plt.plot(x,g)
+plt.plot(x,g)
+plt.axvline(x=0.,color='k')
 
+#R_ij
+plt.axvline(x=8,ymax=0.05,color='r', linestyle='--')
+plt.text(x=7.7, y=-0.02,s="$R_{ij}$", color='k',rotation=-90)
+
+#t_k
+plt.axvline(x=4,ymax=0.25,color='k', linestyle='--')
+plt.text(x=3.7, y=-0.03,s="$t_k-r_{ij}$", color='k',rotation=-90)
+
+plt.axvline(x=5,ymax=0.15,color='k', linestyle='--')
+plt.text(x=4.7, y=-0.02,s="$t_k$", color='k',rotation=-90)
+
+plt.axvline(x=6,ymax=0.085,color='k', linestyle='--')
+plt.text(x=5.7, y=-0.031,s="$t_k+r_{ij}$", color='k',rotation=-90)
+
+
+#fill intersection
+x_intersection=x[ np.logical_and(x<8.0, x>-8.0)]
+y_intersection=g[np.logical_and(x<8.0, x>-8.0)]
+plt.fill(x_intersection,y_intersection, facecolor='orange', alpha=0.5)
+
+x_intersection=x[ np.logical_and(x<6.0, x>4.0)]
+y_intersection=g[np.logical_and(x<6.0, x>4.0)]
+plt.fill_between(x_intersection,(0-y_intersection)*-1, facecolor="blue", alpha=2)
+#hide ticks
+plt.xticks([])
+plt.yticks([])
+
+plt.xlabel("x")
+plt.ylabel("y")
+plt.savefig(r"C:\Gamal Elkoumy\PhD\OneDrive - Tartu Ülikool\Differential Privacy\paper\amun-workshop-paper\figures\guessing_advantage.pdf")
+plt.show()
