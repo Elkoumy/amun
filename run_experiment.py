ebe6b7c9a47ddf477106d327b5c79a13737b0e79
Gamal Elkoumy <gamal.elkomy@gmail.com>
2020-08-24 16:28:07 +0300

    draw process model

--- run_experiment.py
+++ run_experiment.py
@@ -9,7 +9,7 @@ from amun.input_module import *
 import pandas as pd
 from amun.data_visualization import plot_results
 from statistics import median
-
+from amun.model_visualization import view_model
 # DFG as a counter object
 # dfg_freq, dfg_time = read_xes("sample_data/manufacurer.xes")
 
@@ -25,6 +25,7 @@ from statistics import median
 # print(len(dfg_freq))
 # sys.exit()
 
+process_model_dir=r"C:\Gamal Elkoumy\PhD\OneDrive - Tartu Ülikool\Differential Privacy\source code\experiment_figures\process_models"
 data_dir =r"C:\Gamal Elkoumy\PhD\OneDrive - Tartu Ülikool\Data\Data XES"
 # datasets=["CCC19","Sepsis Cases - Event Log","CoSeLoG_WABO_2","BPIC15_2","CreditRequirement","BPIC15_1","Hospital_log","Road_Traffic_Fine_Management_Process"]
 datasets=["Sepsis Cases - Event Log","CoSeLoG_WABO_2","BPIC15_2"]
@@ -67,7 +68,7 @@ for dataset in datasets:
                 # print(epsilon_time)
                 # print(min(epsilon_time.values()))
                 # result_log_delta.append([dataset,i,delta,epsilon_freq,min(epsilon_time.values()), emd_freq, emd_time]) # logging the min epsilon for time as it is the maximum added noise
-
+                view_model(dfg_freq_new,process_model_dir+r"/fig_input_delta_"+str(delta)+"_"+dataset+"_"+str(aggregate_type)+"_"+str(i))
             print("avg EMD for freq is " + str(emd_freq_tot/no_of_experiments))
             print("avg EMD for time is " + str(emd_time_tot/no_of_experiments))
             result_log_delta.append([dataset,aggregate_type,  delta, epsilon_freq, epsilon_time_min/no_of_experiments, emd_freq_tot/no_of_experiments,
@@ -89,7 +90,7 @@ for dataset in datasets:
             for edge in delta_freq_dfg.keys():
                 delta_logger_freq.append([dataset, aggregate_type, emd,delta_freq_dfg[edge]])
             result_log_alpha.append([dataset,aggregate_type,emd,min(list(epsilon_freq.values())), min(list(epsilon_time.values())) , median(list(delta_freq_dfg.values())), median(list(delta_time_dfg.values())) , max(list(delta_freq_dfg.values())), max(list(delta_time_dfg.values())) ])
-
+            view_model(dfg_freq_new, process_model_dir + r"/fig_input_emd_" + str(emd) + "_" + dataset + "_" + str(aggregate_type))
             print("delta for the freq is "+ str(delta_freq))
             print("delta for the time is "+ str(delta_time))
 
