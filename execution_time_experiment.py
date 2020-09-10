d21eda2056a9b7ac8b91a5cfd907d89b4511606d
Gamal Elkoumy <gamal.elkomy@gmail.com>
2020-09-10 11:29:17 +0300

    execution time experiment

--- execution_time_experiment.py
+++ execution_time_experiment.py
@@ -4,27 +4,14 @@ In this file, we run the experiments for different files
 """
 
 from amun.differental_privacy_module import *
-# from GUI_module import *
 from amun.input_module import *
 import pandas as pd
 from amun.data_visualization import plot_results
 from statistics import median
-from amun.model_visualization import view_model
-# DFG as a counter object
-# dfg_freq, dfg_time = read_xes("sample_data/manufacurer.xes")
-
-# dfg_freq, dfg_time = read_xes(r"C:\Gamal Elkoumy\PhD\OneDrive - Tartu Ülikool\Data\Data XES\CCC19.xes")
-# dfg_freq, dfg_time = read_xes(r"C:\Gamal Elkoumy\PhD\OneDrive - Tartu Ülikool\Data\Data XES\Sepsis Cases - Event Log.xes")
-# dfg_freq, dfg_time = read_xes(r"C:\Gamal Elkoumy\PhD\OneDrive - Tartu Ülikool\Data\Data XES\CoSeLoG_WABO_2.xes")
-# dfg_freq, dfg_time = read_xes(r"C:\Gamal Elkoumy\PhD\OneDrive - Tartu Ülikool\Data\Data XES\BPIC15_2.xes")
-# dfg_freq, dfg_time = read_xes(r"C:\Gamal Elkoumy\PhD\OneDrive - Tartu Ülikool\Data\Data XES\CreditRequirement.xes") #strange behavior with time delta
-# dfg_freq, dfg_time = read_xes(r"C:\Gamal Elkoumy\PhD\OneDrive - Tartu Ülikool\Data\Data XES\BPIC15_1.xes")
-# dfg_freq, dfg_time = read_xes(r"C:\Gamal Elkoumy\PhD\OneDrive - Tartu Ülikool\Data\Data XES\Hospital_log.xes")
-# dfg_freq, dfg_time = read_xes(r"C:\Gamal Elkoumy\PhD\OneDrive - Tartu Ülikool\Data\Data XES\Road_Traffic_Fine_Management_Process.xes")
-
-# print(len(dfg_freq))
-# sys.exit()
-
+import time
+import seaborn as sns
+import matplotlib.pyplot as plt
+import os
 process_model_dir=r"C:\Gamal Elkoumy\PhD\OneDrive - Tartu Ülikool\Differential Privacy\source code\experiment_figures\process_models"
 data_dir =r"C:\Gamal Elkoumy\PhD\OneDrive - Tartu Ülikool\Data\Data XES"
 figures_dir=r'C:\Gamal Elkoumy\PhD\OneDrive - Tartu Ülikool\Differential Privacy\source code\experiment_figures'
@@ -32,22 +19,66 @@ figures_dir=r'C:\Gamal Elkoumy\PhD\OneDrive - Tartu Ülikool\Differential Privac
 
 # datasets=["Sepsis Cases - Event Log","CreditRequirement","Road_Traffic_Fine_Management_Process"]
 
-datasets=["Road_Traffic_Fine_Management_Process"]
+datasets=["Sepsis Cases - Event Log","CreditRequirement"]
 
 
 
 no_of_experiments=1
 precision=0.5
+log=[]
 for dataset in datasets:
     aggregate_type = AggregateType.MIN
-
+    print("starting loading data")
     dfg_freq, dfg_time = read_xes(data_dir + "\\" + dataset + ".xes", aggregate_type)
-
+    print("finish loading data")
     delta=0.1
+    start_time = time.time()
     dfg_freq_new, dfg_time_new, epsilon_freq, epsilon_time, MAPE_freq, SMAPE_freq, APE_dist_freq, MAPE_time, SMAPE_time, APE_dist_time = differential_privacy_with_risk(
         dfg_freq, dfg_time, delta=delta, precision=precision, aggregate_type=aggregate_type)
+    end_time = time.time()
+    time_diff_delta=end_time-start_time
+    print("delta time = "+str(end_time-start_time))
+    log.append([dataset, aggregate_type, "delta", time_diff_delta])
+
     emd=0.1
+    start_time = time.time()
     dfg_freq_new, dfg_time_new, epsilon_freq, epsilon_time, delta_freq, delta_time, delta_freq_dfg, delta_time_dfg = differential_privacy_with_accuracy(
         dfg_freq, dfg_time, precision=precision, distance=emd, aggregate_type=aggregate_type)
+    end_time = time.time()
+
+    print("alpha time = "+str(end_time-start_time))
+
+    time_diff_alpha = end_time - start_time
+
+    log.append([dataset, aggregate_type,"alpha",time_diff_alpha])
+
+log.append(["Road_Traffic_Fine_Management_Process", aggregate_type,"delta",3189.7172985076904])
+log.append(["Road_Traffic_Fine_Management_Process", aggregate_type,"alpha",4094.1853733062744])
 
 
+log=pd.DataFrame.from_records(log,columns=["dataset","aggregate_type", "parameter", "time_diff"])
+log.to_csv(r"experiment_logs/execution_time_log.csv", index=False)
+
+# log=pd.read_csv(r"experiment_logs/execution_time_log.csv")
+
+log['dataset'].replace('Road_Traffic_Fine_Management_Process','Traffic Fines',inplace=True)
+log['dataset'].replace('Sepsis Cases - Event Log','Sepsis Cases',inplace=True)
+log['dataset'].replace('CreditRequirement','Credit Requirement',inplace=True)
+
+log['parameter'].replace('delta','Input Delta',inplace=True)
+log['parameter'].replace('alpha','Input Alpha',inplace=True)
+
+
+g=sns.lineplot("dataset", "time_diff","parameter", data=log)
+g.set_ylabel("Execution Time Log(Seconds)")  # epsilon
+g.set_xlabel("Dataset")  # delta
+
+g.set( yscale="log")
+plt.legend()
+plt.savefig(os.path.join(figures_dir, 'execution_time.pdf'))
+plt.show()
+
+""" Execution time for the Road_Traffic_Fine_Management_Process"""
+# alpha time = 4094.1853733062744
+
+# delta time = 3189.7172985076904
