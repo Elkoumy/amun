dd22f99daee7349fc64bb870f509a523cc26dbe0
Gamal Elkoumy <gamal.elkomy@gmail.com>
2020-10-02 18:35:22 +0300

    merge forward and backward optimal branching

--- amun/optimal_branching.py
+++ amun/optimal_branching.py
@@ -1,7 +1,9 @@
 import sys
 
-#replaced sys.maxint to 99999
+#replaced sys.maxint to inf
 # --------------------------------------------------------------------------------- #
+from math import inf
+
 
 def _input(filename):
     prices = {}
@@ -12,6 +14,7 @@ def _input(filename):
         name = int(name.replace('M', ''))
         src = int(src.replace('C', ''))
         dst = int(dst.replace('C', ''))
+
         price = int(price)
         t = (src, dst)
         if t in prices and prices[t] <= price:
@@ -61,7 +64,7 @@ def _getCycle(n, g, visited=None, cycle=None):
 def _mergeCycles(cycle, G, RG, g, rg):
     allInEdges = []
     minInternal = None
-    minInternalWeight = 99999
+    minInternalWeight = inf
 
     # find minimal internal edge weight
     for n in cycle:
@@ -151,7 +154,7 @@ def mst(root, G):
     for n in RG:
         if len(RG[n]) == 0:
             continue
-        minimum = 99999
+        minimum = inf
         s, d = None, None
         for e in RG[n]:
             if RG[n][e] < minimum:
@@ -178,7 +181,74 @@ def mst(root, G):
     return g
 
 
+def transform_dfg(dfg):
+    names={}
+    result={}
+    reverse_result={}
+    idx=0
+    for ix, key in enumerate(list(dfg.keys())):
+        activity1, activity2=0,0
+        if key[0] in names:
+            activity1=names[key[0]]
+        else:
+            names[key[0]]=idx
+            activity1=idx
+            idx+=1
+
+        if key[1] in names:
+            activity2=names[key[1]]
+        else:
+            names[key[1]]=idx
+            activity2=idx
+            idx+=1
+
+        result[(activity1,activity2)]=dfg[key]
+        reverse_result[(activity2, activity1)] = dfg[key]
+
+    return names,result, reverse_result
+
+
+
+def get_optimal_branching(dfg, root, tail):
+    names, result, reverse_result = transform_dfg(dfg)
+
+    root = "11"
+    tail = ""
+    g_forward = _load(result, result)
+    h_forward = mst(int(root), g_forward)
+
+    g_backward = _load(reverse_result, reverse_result)
+    h_backward = mst(int(tail), g_backward)
+
+    result=merge_graphs(h_forward, h_backward, names)
+
+    for s in h_forward:
+        for t in h_forward[s]:
+            print("%d-%d" % (s, t))
+
 # --------------------------------------------------------------------------------- #
+
+def find_key(dictionary, value):
+    for name,idx in dictionary.items():  # for name, age in dictionary.iteritems():  (for Python 2.x)
+        if idx == value:
+            return name
+    return -1
+def merge_graphs(result, reverse_result,names):
+    dfg={}
+    renamed={}
+    for s in result:
+        for t in result[s]:
+            dfg[(s,t)]=result[s][t]
+    for s in reverse_result:
+        for t in reverse_result[s]:
+            if (t,s) not in list(dfg.keys()):
+                dfg[(t,s)] =reverse_result[s][t]
+
+    for key in list(dfg.keys()):
+        renamed[(find_key(names,key[0]), find_key(names,key[1]))]= dfg[key]
+
+    return renamed
+
 
 if __name__ == "__main__":
     # try:
@@ -190,20 +260,32 @@ if __name__ == "__main__":
     #     sys.exit(1)
 
     # prices, names = _input(filename)
-    prices, names = _input('edges')
+    # prices, names = _input('edges')
+
+    names={(1, 2): 1, (1, 5): 2, (1, 8): 3, (2, 3): 4, (3, 6): 5, (3, 4): 6, (4, 11): 7, (5, 2): 8, (5, 6): 9, (6, 8): 10,
+     (6, 7): 11, (7, 3): 12, (7, 11): 13, (7, 10): 14, (8, 9): 15, (9, 10): 16, (9, 7): 17, (10, 11): 18, (10, 10): 18}
+    prices={(1, 2): 5, (1, 5): 10, (1, 8): 5, (2, 3): 10, (3, 6): 25, (3, 4): 10, (4, 11): 5, (5, 2): 15, (5, 6): 20, (6, 8): 5, (6, 7): 30, (7, 3): 15, (7, 11): 15, (7, 10): 5, (8, 9): 10, (9, 10): 10, (9, 7): 5, (10, 11): 10, (10, 10): 40}
     root="1"
     g = _load(prices, prices)
-    h = mst(int(root), g)
-    for s in h:
-        for t in h[s]:
+    h1 = mst(int(root), g)
+    for s in h1:
+        for t in h1[s]:
             print("%d-%d" % (s, t))
 
     print("****** Reversed ********")
     """" reversed way"""
-    prices, names = _input('edges_reversed')
+    # prices, names = _input('edges_reversed')
+    dfg= {('Leucocytes', 'CRP'): 1778, ('ER Registration', 'ER Triage'): 971, ('ER Triage', 'ER Sepsis Triage'): 905, ('LacticAcid', 'Leucocytes'): 565, ('IV Liquid', 'IV Antibiotics'): 501, ('IV Antibiotics', 'Admission NC'): 489, ('Admission NC', 'Leucocytes'): 408, ('CRP', 'Release A'): 322, ('ER Sepsis Triage', 'IV Liquid'): 285, ('Release A', 'Return ER'): 276, ('IV Antibiotics', 'Admission IC'): 46, ('Admission IC', 'LacticAcid'): 41, ('CRP', 'Release B'): 19, ('CRP', 'Release C'): 13, ('CRP', 'Release D'): 12, ('CRP', 'Release E'): 3}
+
+    names={(2, 1): 1, (5, 1): 2, (8, 1): 3, (3, 2): 4, (6, 3): 5, (4, 3): 6, (11, 4): 7, (2, 5): 8, (6, 5): 9, (8, 6): 10, (7, 6): 11, (3, 7): 12, (11, 7): 13, (10, 7): 14, (9, 8): 15, (10, 9): 16, (7, 9): 17, (11, 10): 18}
+    names, result, reverse_result=transform_dfg(dfg)
+
+    prices={(2, 1): 5, (5, 1): 10, (8, 1): 5, (3, 2): 10, (6, 3): 25, (4, 3): 10, (11, 4): 5, (2, 5): 15, (6, 5): 20, (8, 6): 5, (7, 6): 30, (3, 7): 15, (11, 7): 15, (10, 7): 5, (9, 8): 10, (10, 9): 10, (7, 9): 5, (11, 10): 10}
     root="11"
     g = _load(prices, prices)
-    h = mst(int(root), g)
-    for s in h:
-        for t in h[s]:
+    h2 = mst(int(root), g)
+
+    merged = merge_graphs(h1,h2, names)
+    for s in h2:
+        for t in h2[s]:
             print("%d-%d" % (s, t))
--- run_experiment.py
+++ run_experiment.py
@@ -31,7 +31,7 @@ figures_dir=r'C:\Gamal Elkoumy\PhD\OneDrive - Tartu Ülikool\Differential Privac
 # datasets=["CCC19","Sepsis Cases - Event Log","CoSeLoG_WABO_2","BPIC15_2","CreditRequirement","BPIC15_1","Hospital_log","Road_Traffic_Fine_Management_Process"]
 
 # datasets=["Sepsis Cases - Event Log","CreditRequirement","Road_Traffic_Fine_Management_Process"]
-datasets=["CreditRequirement"]
+datasets=["Sepsis Cases - Event Log"]
 # datasets=["Road_Traffic_Fine_Management_Process"]
 result_log_delta = []  # holds the delta as input exeperiment
 # vales is exp_index, delta, epsilon_freq, epsilon_time, emd_freq, emd_time
