13fe500e2fbc0cf6736096ca5a2caf1b36e59b6a
Gamal Elkoumy <gamal.elkomy@gmail.com>
2020-10-22 14:45:47 +0300

    adding runtime experiment

--- execution_time_experiment.py
+++ execution_time_experiment.py
@@ -4,81 +4,80 @@ In this file, we run the experiments for different files
 """
 
 from amun.differental_privacy_module import *
+# from GUI_module import *
 from amun.input_module import *
 import pandas as pd
 from amun.data_visualization import plot_results
 from statistics import median
-import time
-import seaborn as sns
-import matplotlib.pyplot as plt
+from amun.model_visualization import view_model
 import os
-process_model_dir=r"C:\Gamal Elkoumy\PhD\OneDrive - Tartu Ülikool\Differential Privacy\source code\experiment_figures\process_models"
-data_dir =r"C:\Gamal Elkoumy\PhD\OneDrive - Tartu Ülikool\Data\Data XES"
-figures_dir=r'C:\Gamal Elkoumy\PhD\OneDrive - Tartu Ülikool\Differential Privacy\source code\experiment_figures'
+import time
 
 
-# datasets=["Sepsis Cases - Event Log","CreditRequirement","Road_Traffic_Fine_Management_Process"]
-
-datasets=["Sepsis Cases - Event Log","CreditRequirement"]
-
-
-
-no_of_experiments=1
-precision=0.5
-log=[]
-for dataset in datasets:
-    aggregate_type = AggregateType.MIN
-    print("starting loading data")
-    dfg_freq, dfg_time = read_xes(data_dir + "\\" + dataset + ".xes", aggregate_type)
-    print("finish loading data")
-    delta=0.1
+def run_experiment(data="Sepsis", parameter="0.1", mode="nonpruning", aggregate_type=AggregateType.AVG,
+                   input_val="delta"):
+    """Parameters to the script"""
     start_time = time.time()
-    dfg_freq_new, dfg_time_new, epsilon_freq, epsilon_time, MAPE_freq, SMAPE_freq, APE_dist_freq, MAPE_time, SMAPE_time, APE_dist_time = differential_privacy_with_risk(
-        dfg_freq, dfg_time, delta=delta, precision=precision, aggregate_type=aggregate_type)
-    end_time = time.time()
-    time_diff_delta=end_time-start_time
-    print("delta time = "+str(end_time-start_time))
-    log.append([dataset, aggregate_type, "delta", time_diff_delta])
+    input_dataset = data
+    input_alpha_delta = float(parameter)
+    '''**********************'''
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    process_model_dir = os.path.join(dir_path, "experiment_figures", "process_models")
+    data_dir = os.path.join(dir_path, "data")
+    figures_dir = os.path.join(dir_path, 'experiment_figures')
+    log_dir = os.path.join(dir_path, 'experiment_logs')
 
-    emd=0.1
-    start_time = time.time()
-    dfg_freq_new, dfg_time_new, epsilon_freq, epsilon_time, delta_freq, delta_time, delta_freq_dfg, delta_time_dfg = differential_privacy_with_accuracy(
-        dfg_freq, dfg_time, precision=precision, distance=emd, aggregate_type=aggregate_type)
-    end_time = time.time()
+    precision = 0.5
+    dataset = input_dataset
 
-    print("alpha time = "+str(end_time-start_time))
+    print("Dataset: " + dataset)
+    print("Aggregate Type: " + str(aggregate_type))
+    dfg = read_xes(data_dir, dataset, aggregate_type, mode)
 
+    if input_val == "delta":
+        delta = input_alpha_delta
+
+        dfg_new, dfg_new, epsilon, MAPE, SMAPE, APE_dist = differential_privacy_with_risk(dfg, delta=delta,
+                                                                                          precision=precision,
+                                                                                          aggregate_type=aggregate_type)
+        end_time = time.time()
+
+
+
+    else:
+
+        emd = input_alpha_delta
+
+        dfg_new, epsilon, delta, delta_dfg = differential_privacy_with_accuracy(dfg, precision=precision,
+                                                                                distance=emd,
+                                                                                aggregate_type=aggregate_type)
+        end_time = time.time()
+
+    print("time = " + str(end_time - start_time))
     time_diff_alpha = end_time - start_time
-
-    log.append([dataset, aggregate_type,"alpha",time_diff_alpha])
-
-log.append(["Road_Traffic_Fine_Management_Process", aggregate_type,"delta",3189.7172985076904])
-log.append(["Road_Traffic_Fine_Management_Process", aggregate_type,"alpha",4094.1853733062744])
-
-
-log=pd.DataFrame.from_records(log,columns=["dataset","aggregate_type", "parameter", "time_diff"])
-log.to_csv(r"experiment_logs/execution_time_log.csv", index=False)
-
-# log=pd.read_csv(r"experiment_logs/execution_time_log.csv")
-
-log['dataset'].replace('Road_Traffic_Fine_Management_Process','Traffic Fines',inplace=True)
-log['dataset'].replace('Sepsis Cases - Event Log','Sepsis Cases',inplace=True)
-log['dataset'].replace('CreditRequirement','Credit Requirement',inplace=True)
-
-log['parameter'].replace('delta','Input Delta',inplace=True)
-log['parameter'].replace('alpha','Input Alpha',inplace=True)
+    log = []
+    log.append([dataset, parameter, mode, aggregate_type, input_val, time_diff_alpha])
+    log.to_csv(os.path.join(log_dir, "execution_time_%s_%s_%s_%s_%s.csv" % (
+        input_dataset, str(input_alpha_delta), mode, aggregate_type, input_val)), index=False)
 
 
-g=sns.lineplot("dataset", "time_diff","parameter", data=log)
-g.set_ylabel("Execution Time Log(Seconds)")  # epsilon
-g.set_xlabel("Dataset")  # delta
+if __name__ == "__main__":
 
-g.set( yscale="log")
-plt.legend()
-plt.savefig(os.path.join(figures_dir, 'execution_time.pdf'))
-plt.show()
+    data = os.sys.argv[1]
+    parameter = os.sys.argv[2]
+    mode = os.sys.argv[3]
+    aggregate_type = os.sys.argv[4]
+    input_val = os.sys.argv[5]
 
-""" Execution time for the Road_Traffic_Fine_Management_Process"""
-# alpha time = 4094.1853733062744
+    if aggregate_type == "AggregateType.FREQ":
+        aggregate_type = AggregateType.FREQ
+    elif aggregate_type == "AggregateType.MIN":
+        aggregate_type = AggregateType.MIN
+    elif aggregate_type == "AggregateType.MAX":
+        aggregate_type = AggregateType.MAX
+    elif aggregate_type == "AggregateType.AVG":
+        aggregate_type = AggregateType.AVG
+    elif aggregate_type == "AggregateType.SUM":
+        aggregate_type = AggregateType.SUM
 
-# delta time = 3189.7172985076904
+    run_experiment(data=data, parameter=parameter, mode=mode, aggregate_type=aggregate_type, input_val=input_val)
