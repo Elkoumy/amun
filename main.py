37e8f55d71c8c26633fd792f6792b80dcb9f0815
Gamal Elkoumy <gamal.elkomy@gmail.com>
2020-10-23 18:12:04 +0300

    run rest of files

--- main.py
+++ main.py
@@ -8,8 +8,9 @@ jobs_dir = "jobs"
 
 # datasets=["BPIC12","BPIC13","BPIC15","BPIC17","BPIC18","BPIC19","BPIC20","CCC19","CreditReq","Hospital","Sepsis","Traffic","Unrineweginfectie", "BPIC14"]
 # datasets=["CCC19","Sepsis","Unrineweginfectie", "BPIC14","Traffic","Hospital","CreditReq","BPIC20","BPIC12","BPIC13","BPIC15","BPIC17","BPIC18","BPIC19"]
-# datasets=["Sepsis","Unrineweginfectie","Hospital","CreditReq","BPIC20","BPIC12","BPIC13","BPIC15","BPIC17"]
-datasets=["Traffic","BPIC18","BPIC19"]
+# datasets=["Sepsis","Unrineweginfectie","Hospital","BPIC20","BPIC12"]
+datasets=["CreditReq","BPIC13","BPIC15"]
+# datasets=["BPIC17","Traffic","BPIC18","BPIC19"]
 parameters=[0.01,0.05, 0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9]
 aggregate_types = [ AggregateType.FREQ,AggregateType.AVG, AggregateType.SUM,AggregateType.MIN,AggregateType.MAX]
 input_values=["delta","alpha"]
