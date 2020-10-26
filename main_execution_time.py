4cbb7b0eeeb5b5b2d44d5f7f5a5b886e95cc8a2f
Gamal Elkoumy <gamal.elkomy@gmail.com>
2020-10-26 09:46:08 +0200

    run BPIC18,19 execution time

--- main_execution_time.py
+++ main_execution_time.py
@@ -7,8 +7,8 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 jobs_dir = "jobs"
 
 # datasets=["BPIC12","BPIC13","BPIC15","BPIC17","BPIC18","BPIC19","BPIC20","CCC19","CreditReq","Hospital","Sepsis","Traffic","Unrineweginfectie", "BPIC14"]
-datasets=["CCC19","Sepsis","Unrineweginfectie", "BPIC14","Traffic","Hospital","CreditReq","BPIC20","BPIC12","BPIC13","BPIC15","BPIC17","BPIC18","BPIC19"]
-# datasets=["BPIC12","BPIC13","BPIC20","CreditReq"]
+# datasets=["CCC19","Sepsis","Unrineweginfectie", "BPIC14","Traffic","Hospital","CreditReq","BPIC20","BPIC12","BPIC13","BPIC15","BPIC17","BPIC18","BPIC19"]
+datasets=["BPIC18","BPIC19"]
 # datasets=["Sepsis"]
 parameters=[0.01]
 aggregate_types = [AggregateType.AVG]
@@ -72,7 +72,7 @@ for data in datasets:
                         if data=="Traffic":
                             fout.write("#SBATCH --partition=amd\n")
                         else:
-                            fout.write("#SBATCH --partition=main\n")
+                            fout.write("#SBATCH --partition=amd\n")
                         fout.write("#SBATCH --time=%s\n" % (exec_time))
                         # fout.write("cd ..\n")
                         fout.write("python -u %s \"%s\" %s \"%s\" \"%s\" \"%s\" \n" % ('"'+os.path.join(dir_path,"execution_time_experiment.py")+'"', data, parameter,mode, aggregate_type,input_value))  # hyper_param_optim
