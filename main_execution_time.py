3eb6348f9ac14f67e1f29f095b8a8278d922bddb
Gamal Elkoumy <gamal.elkomy@gmail.com>
2020-11-02 15:12:12 +0200

    Update main_execution_time.py

--- main_execution_time.py
+++ main_execution_time.py
@@ -8,9 +8,9 @@ jobs_dir = "jobs"
 
 # datasets=["BPIC12","BPIC13","BPIC15","BPIC17","BPIC18","BPIC19","BPIC20","CCC19","CreditReq","Hospital","Sepsis","Traffic","Unrineweginfectie", "BPIC14"]
 # datasets=["Sepsis","Unrineweginfectie", "BPIC14","Traffic","Hospital","CreditReq","BPIC20","BPIC12","BPIC13","BPIC15","BPIC17","BPIC18","BPIC19"]
-# datasets=["BPIC18","BPIC19","BPIC17"]
+datasets=["BPIC18","BPIC19","BPIC17"]
 # datasets=["BPIC14"]
-datasets=["BPIC19"]
+# datasets=["BPIC19"]
 # datasets=["Sepsis"]
 parameters=[0.01]
 aggregate_types = [AggregateType.AVG]
