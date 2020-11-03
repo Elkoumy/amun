c4f5d1e00f48351f87d6d316f36d7d810a54d7bc
Gamal Elkoumy <gamal.elkomy@gmail.com>
2020-11-03 11:16:11 +0200

    mem per cpu

--- main_execution_time.py
+++ main_execution_time.py
@@ -72,8 +72,8 @@ for data in datasets:
                     with open(job_name, "w") as fout:
                         fout.write("#!/bin/bash\n")
                         fout.write("#SBATCH --output=jobs/time_log_%s_%s_%s_%s_%s.txt\n" % (data, parameter,mode,aggregate_type,input_value))
-                        fout.write("#SBATCH --mem=%sGB\n" % memory)
-                        # fout.write("#SBATCH --mem-per-cpu=%sGB\n" % int(memory))
+                        # fout.write("#SBATCH --mem=%sGB\n" % memory)
+                        fout.write("#SBATCH --mem-per-cpu=%sGB\n" % int(memory))
                         fout.write("#SBATCH --ntasks=1\n")  ## Run on a single CPU
                         fout.write("#SBATCH --cpus-per-task=4\n")  # 4 cores per cpu
 
