bbf12fea59d8cb3c7af341c8658d1222cb444cb2
Gamal Elkoumy <gamal.elkomy@gmail.com>
2020-11-05 10:57:08 +0200

    Update test_parallel_job.sh

--- test_parallel_job.sh
+++ test_parallel_job.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 #SBATCH --output=test_parallel_log.txt
-#SBATCH --mem=4GB
+#SBATCH --mem=16GB
 #SBATCH --ntasks=4
 #SBATCH --partition=main
 #SBATCH --time=02:00:00
 module load python-3.7.1
-python -u "/gpfs/hpc/home/elkoumy/amun/test_parallel.py"
+#python -u "/gpfs/hpc/home/elkoumy/amun/test_parallel.py"
+python -u "/gpfs/hpc/home/elkoumy/amun/execution_time_experiment.py" "BPIC17" 0.01 "nonpruning" "AggregateType.MIN" "delta"
