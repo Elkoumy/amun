1e333c0cb27b183776420d07388acbb2372d42fa
Gamal Elkoumy <gamal.elkomy@gmail.com>
2020-11-18 00:02:37 +0200

    update logo

--- amun/data_visualization.py
+++ amun/data_visualization.py
@@ -389,12 +389,12 @@ execution_time_log=pd.read_csv(os.path.join('../experiment_logs', "execution_tim
 #
 # #
 dir=r'C:\Gamal Elkoumy\PhD\OneDrive - Tartu Ülikool\Differential Privacy\source code\experiment_figures'
-# result_log_delta=pd.read_csv(os.path.join('../experiment_logs', "combined_result_log_delta_subsetted.csv"))
-# result_log_alpha=pd.read_csv(os.path.join('../experiment_logs', "combined_result_log_alpha.csv"))
-# bubble_heatmap(result_log_delta,result_log_alpha)
+result_log_delta=pd.read_csv(os.path.join('../experiment_logs', "combined_result_log_delta_subsetted.csv"))
+result_log_alpha=pd.read_csv(os.path.join('../experiment_logs', "combined_result_log_alpha.csv"))
+bubble_heatmap(result_log_delta,result_log_alpha)
 # paper_distributions(combined_delta_logger,dir)
 # plot_input_delta(result_log_delta,dir)
-plot_execution_time(execution_time_log,dir)
+# plot_execution_time(execution_time_log,dir)
 # # plot_results(result_log_delta,result_log_alpha,delta_logger_freq,delta_logger_time)
 #
 # # plot_input_alpha(result_log_alpha,dir)
