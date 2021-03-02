62de383710db8bd856a65a5f2e7072f701d38c22
Gamal Elkoumy <gamal.elkomy@gmail.com>
2021-03-02 10:32:34 +0200

    TODOs for XES exporter

--- amun/log_exporter.py
+++ amun/log_exporter.py
@@ -8,6 +8,10 @@ def relative_time_to_XES(data,out_dir):
     :return: It exports the event log as XES to the specified directory.
     """
 
+    #TODO: get the timestamp of the first event
+
+    #TODO: event_timestamp= prev+relative ===> accumulation per case
+
     # log = conversion_factory.apply(data[['case:concept:name','concept:name','time:timestamp']])
     # xes_exporter.export_log(log, os.path.join(data_dir,dataset+"_anonymized.xes"))
 
