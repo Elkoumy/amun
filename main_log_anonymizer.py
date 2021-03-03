af15a54b5ccb1516c27e4e93b7c1578f1701edb1
Gamal Elkoumy <gamal.elkomy@gmail.com>
2021-03-04 00:11:55 +0200

    run BPIC18

--- main_log_anonymizer.py
+++ main_log_anonymizer.py
@@ -16,10 +16,10 @@ jobs_dir = "jobs"
 datasets = ["CCC19_t", "Sepsis_t", "Unrineweginfectie_t", "BPIC14_t", "Traffic_t", "Hospital_t", "CreditReq_t", "BPIC20_t",
                 "BPIC12_t", "BPIC13_t", "BPIC15_t", "BPIC17_t", "BPIC18_t", "BPIC19_t"]
 
-datasets = ["CCC19_t", "Sepsis_t", "Unrineweginfectie_t",  "Traffic_t", "Hospital_t", "CreditReq_t", "BPIC20_t",
-                "BPIC12_t", "BPIC13_t", "BPIC15_t", "BPIC17_t"]
+# datasets = ["CCC19_t", "Sepsis_t", "Unrineweginfectie_t",  "Traffic_t", "Hospital_t", "CreditReq_t", "BPIC20_t",
+#                 "BPIC12_t", "BPIC13_t", "BPIC15_t", "BPIC17_t"]
 
-# datasets = ["temp"]
+datasets = ["BPIC18_t"]
 
 memory = 4
 exec_time="01:00:00" # 1 hour
