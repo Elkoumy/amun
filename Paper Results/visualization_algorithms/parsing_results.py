3ba70b03d8f421f0dd2dc2d81e23b0718bc0d730
Gamal Elkoumy <gamal.elkomy@gmail.com>
2021-03-06 19:59:05 +0200

    run 3rd iteration on HPC

--- Paper Results/visualization_algorithms/parsing_results.py
+++ Paper Results/visualization_algorithms/parsing_results.py
@@ -1,14 +1,6 @@
 import pandas as pd
 
 
-def row_transform(rows, datasets):
-    res = str(rows.delta[0])+"|"
-    vals=[]
-    for i in datasets:
-        vals.append(rows.loc[rows.dataset==i,"smape_time"])
-
-    res+="|".join(vals)
-    return res
 
 
 data=pd.read_csv("../error_metrics/all_error.csv",
@@ -16,11 +8,14 @@ data=pd.read_csv("../error_metrics/all_error.csv",
                      "oversampling_ratio","eps","eps_trace"])
 
 data=data.groupby(["dataset","precision","delta"]).mean().reset_index()
+
 #TODO: filter the precision in case of multiple precisions
 data=data[["dataset","delta","smape_time","oversampling_ratio"]]
 
 t=pd.pivot_table(data, values = 'smape_time', index=['delta'], columns = 'dataset').reset_index()
-# res=data.groupby(['delta']).apply(row_transform,datasets=dataset_names)
+
 t.to_csv("results.csv",index=False, header=True)
+
+
 print("text")
 
