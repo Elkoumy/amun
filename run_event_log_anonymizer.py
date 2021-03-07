3ac00ddf5e90e003025742c39cf7798860f5bf4e
Gamal Elkoumy <gamal.elkomy@gmail.com>
2021-03-07 23:38:31 +0200

    run BPIC18 execution time experiment

--- run_event_log_anonymizer.py
+++ run_event_log_anonymizer.py
@@ -109,7 +109,7 @@ if __name__ == "__main__":
     # precisions=[0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9]
 
     precisions=[0.1]
-    deltas=[0.4]
+    deltas=[0.2]
 
     # datasets = ["CCC19_t",  "Unrineweginfectie_t", "Sepsis_t","Traffic_t", "Hospital_t", "CreditReq_t", "BPIC15_t","BPIC20_t", "BPIC13_t",
     # "BPIC12_t", "BPIC17_t", "BPIC14_t", "BPIC19_t", "BPIC18_t" ]
