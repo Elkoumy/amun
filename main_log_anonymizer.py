5aa35c33a7dd118ef19098299a0cf215087890f1
Gamal Elkoumy <gamal.elkomy@gmail.com>
2021-03-08 18:07:15 +0200

    run 2 iterations on HPC.

--- main_log_anonymizer.py
+++ main_log_anonymizer.py
@@ -30,8 +30,8 @@ datasets = ["CCC19_t",  "Unrineweginfectie_t", "Sepsis_t","Traffic_t", "Hospital
 memory = 4
 exec_time="01:00:00" # 1 hour
 
-no_of_iterations =8
-start_iteration=6
+no_of_iterations =10
+start_iteration=8
 # no_of_iterations =1
 # start_iteration=0
 
