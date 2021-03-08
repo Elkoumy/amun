4f0be0cd61a1fba16dff92f380e7863582579ba8
Gamal Elkoumy <gamal.elkomy@gmail.com>
2021-03-08 22:45:37 +0200

    run single iteration BPIC18

--- main_log_anonymizer.py
+++ main_log_anonymizer.py
@@ -24,22 +24,22 @@ jobs_dir = "jobs"
 # datasets = ["CCC19_t",  "Unrineweginfectie_t", "Sepsis_t","Traffic_t", "Hospital_t", "CreditReq_t", "BPIC15_t","BPIC20_t", "BPIC13_t",
 # "BPIC12_t", "BPIC17_t"]
 
-datasets =[ "BPIC19_t"]
+datasets =[ "BPIC18_t"]
 
 # datasets =["BPIC18_t", "BPIC14_t", "BPIC19_t" ]
 
 memory = 4
 exec_time="01:00:00" # 1 hour
 
-no_of_iterations =3
-start_iteration=0
-# no_of_iterations =1
+# no_of_iterations =3
 # start_iteration=0
+no_of_iterations =1
+start_iteration=0
 
 deltas = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7]
 
 precisions = [0.1]
-# deltas=[0.1]
+deltas=[0.1]
 
 for precision in precisions:
     for delta in deltas:
