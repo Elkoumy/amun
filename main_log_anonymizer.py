2bb190c4b701505cde0b3de79ba64b6b94017695
Gamal Elkoumy <gamal.elkomy@gmail.com>
2021-03-10 09:32:39 +0200

    run 1 iteration of precision experiment on main HPC [BPIC18]

--- main_log_anonymizer.py
+++ main_log_anonymizer.py
@@ -30,7 +30,7 @@ jobs_dir = "jobs"
 
 # datasets = ["BPIC13_t", "BPIC12_t", "BPIC17_t"]
 
-datasets=["BPIC14_t"]
+datasets=["BPIC18_t"]
 
 memory = 4
 exec_time="01:00:00" # 1 hour
@@ -38,7 +38,7 @@ exec_time="01:00:00" # 1 hour
 # no_of_iterations =14
 # start_iteration=13
 
-no_of_iterations =10
+no_of_iterations =1
 start_iteration=0
 
 # deltas = [0.1, 0.2, 0.3, 0.4, 0.5]
