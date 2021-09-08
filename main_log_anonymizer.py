cccf585af8deed4dad202bcdbc8fee34fb4b20e1
Gamal Elkoumy <gamal.elkomy@gmail.com>
2021-09-08 10:27:05 +0300

    Rerun BPIC19 on HPC.

--- main_log_anonymizer.py
+++ main_log_anonymizer.py
@@ -29,7 +29,7 @@ jobs_dir = "jobs"
 #                 "BPIC20_t",  "BPIC17_t", "BPIC14_t", "BPIC19_t","BPIC18_t" ]
 
 
-datasets=["BPIC18_t"]
+datasets=["BPIC19_t"]
 
 memory = 4
 exec_time="01:00:00" # 1 hour
@@ -43,9 +43,9 @@ start_iteration=0
 # deltas = [0.1, 0.2, 0.3, 0.4, 0.5]
 precisions = [ 0.2]
 # modes=['oversampling','filtering','sampling']
-modes=['oversampling']
+modes=['oversampling','sampling']
 # precisions = [0.1]
-deltas=[0.2,0.3,0.4]
+deltas=[0.2]
 
 for precision in precisions:
     for delta in deltas:
