7b54a328bbfaf7940664d5bb6ab2404949e2514d
Gamal Elkoumy <gamal.elkomy@gmail.com>
2021-09-13 12:33:58 +0300

    Rerun Jaccard Distance BPIC14

--- amun/jaccard_distance/jaccard_distance.py
+++ amun/jaccard_distance/jaccard_distance.py
@@ -28,23 +28,23 @@ def similarity_apply(row):
 import swifter
 
 def soft_intersection_list(tokens1, tokens2):
-    start = time.time()
-    intersected_list = [((token1, token2), similarity(token1, token2)) for token1, token2 in product(tokens1, tokens2)]
-
-    intersected_list = sorted(intersected_list, key=lambda item: item[1], reverse=True)
-
-    included_list = set()
-    used_tokens1 = set()
-    used_tokens2 = set()
-    for (token1, token2), sim in intersected_list:
-        if (not (token1 in used_tokens1)) and (not (token2 in used_tokens2)):
-            included_list.add(((token1, token2), sim))
-            used_tokens1.add(token1)
-            used_tokens2.add(token2)
-
-    end = time.time()
-    diff = end - start
-    print("intersected_list (list): %s (minutes)" % (diff / 60.0))
+    # start = time.time()
+    # intersected_list = [((token1, token2), similarity(token1, token2)) for token1, token2 in product(tokens1, tokens2)]
+    #
+    # intersected_list = sorted(intersected_list, key=lambda item: item[1], reverse=True)
+    #
+    # included_list = set()
+    # used_tokens1 = set()
+    # used_tokens2 = set()
+    # for (token1, token2), sim in intersected_list:
+    #     if (not (token1 in used_tokens1)) and (not (token2 in used_tokens2)):
+    #         included_list.add(((token1, token2), sim))
+    #         used_tokens1.add(token1)
+    #         used_tokens2.add(token2)
+    #
+    # end = time.time()
+    # diff = end - start
+    # print("intersected_list (list): %s (minutes)" % (diff / 60.0))
 
 
 
