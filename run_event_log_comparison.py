162e82d8ecb9fdc8ef50224b8fa667a7e1420dca
Gamal Elkoumy <gamal.elkomy@gmail.com>
2021-09-13 16:26:22 +0300

    Rerun Jaccard Distance BPIC18,19

--- run_event_log_comparison.py
+++ run_event_log_comparison.py
@@ -49,7 +49,7 @@ if __name__ == "__main__":
     # datasets = ["CCC19_t", "Sepsis_t", "Unrineweginfectie_t", "BPIC14_t", "Traffic_t", "Hospital_t", "CreditReq_t",
     #             "BPIC20_t",
     #             "BPIC12_t", "BPIC13_t", "BPIC15_t", "BPIC17_t", "BPIC18_t", "BPIC19_t"]
-    datasets=['BPIC14_t']
+    datasets=['BPIC19_t',"BPIC18_t"]
     dir_path = os.path.dirname(os.path.realpath(__file__))
     comparison_dir = os.path.join(dir_path, "comparison")
     amun_dir=os.path.join(dir_path,"anonymized_logs","amun")
