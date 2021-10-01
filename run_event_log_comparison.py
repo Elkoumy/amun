17a5d740b841a949da4beb45e9a1b401fbe17298
Gamal Elkoumy <gamal.elkomy@gmail.com>
2021-10-01 11:11:40 +0300

    Re-evaluate PRIPEL

--- run_event_log_comparison.py
+++ run_event_log_comparison.py
@@ -50,12 +50,13 @@ if __name__ == "__main__":
     #             "BPIC20_t",
     #             "BPIC12_t", "BPIC13_t", "BPIC15_t", "BPIC17_t", "BPIC18_t", "BPIC19_t"]
 
-    datasets = [  "BPIC14_t"]
+    datasets = [  "BPIC18_t"]
     # datasets=["Unrineweginfectie_t"]
     dir_path = os.path.dirname(os.path.realpath(__file__))
     comparison_dir = os.path.join(dir_path, "comparison")
     amun_dir=os.path.join(dir_path,"anonymized_logs","amun")
     pripel_trace_dir = os.path.join(dir_path, "anonymized_logs", "pripel","trace_variant")
+    pripel_trace_dir = os.path.join(dir_path, "anonymized_logs", "pripel", "new_events")
     pripel_time_dir = os.path.join(dir_path, "anonymized_logs", "pripel","time")
     SaCoFa_trace_dir = os.path.join(dir_path, "anonymized_logs", "SaCoFa")
 
@@ -72,11 +73,11 @@ if __name__ == "__main__":
         #         compare_emd(org_path,anonymized_dir,comparison_dir)
         #         compare_jaccard(org_path, anonymized_dir, comparison_dir)
 
-        # files = list(os.walk(pripel_trace_dir))[0][2]
-        # for log in files:
-        #     if log.find(dataset)!=-1:
-        #         anonymized_dir = os.path.join(pripel_trace_dir, log)
-        #         compare_jaccard(org_path, anonymized_dir, comparison_dir)
+        files = list(os.walk(pripel_trace_dir))[0][2]
+        for log in files:
+            if log.find(dataset)!=-1:
+                anonymized_dir = os.path.join(pripel_trace_dir, log)
+                compare_jaccard(org_path, anonymized_dir, comparison_dir)
         #
         # files = list(os.walk(pripel_time_dir))[0][2]
         # for log in files:
@@ -84,10 +85,10 @@ if __name__ == "__main__":
         #         anonymized_dir = os.path.join(pripel_time_dir, log)
         #         compare_emd(org_path, anonymized_dir, comparison_dir)
 
-        files = list(os.walk(SaCoFa_trace_dir))[0][2]
-        for log in files:
-            if log.find(dataset) != -1:
-                anonymized_dir = os.path.join(SaCoFa_trace_dir, log)
-                compare_jaccard(org_path, anonymized_dir, comparison_dir)
-                # compare_emd(org_path, anonymized_dir, comparison_dir)
+        # files = list(os.walk(SaCoFa_trace_dir))[0][2]
+        # for log in files:
+        #     if log.find(dataset) != -1:
+        #         anonymized_dir = os.path.join(SaCoFa_trace_dir, log)
+        #         compare_jaccard(org_path, anonymized_dir, comparison_dir)
+        #         # compare_emd(org_path, anonymized_dir, comparison_dir)
 
