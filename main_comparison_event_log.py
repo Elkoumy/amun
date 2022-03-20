10dc555faef9070130946bf486415c6f3c36055f
elkoumy <gamal.elkomy@gmail.com>
2022-03-20 11:47:52 +0200

    running EMD and JD

--- main_comparison_event_log.py
+++ main_comparison_event_log.py
@@ -53,35 +53,46 @@ if __name__ == "__main__":
     # datasets = ["CCC19_t", "Unrineweginfectie_t", "BPIC14_t", "Traffic_t", "Hospital_t", "CreditReq_t",
     #             "BPIC20_t",
     #             "BPIC12_t", "BPIC13_t", "BPIC15_t", "BPIC17_t", "BPIC18_t", "BPIC19_t"]
-    datasets=["Traffic_t"]
+    datasets=["Sepsis_t"]
     dir_path = os.path.dirname(os.path.realpath(__file__))
     comparison_dir = os.path.join(dir_path, "comparison")
     # amun_dir = os.path.join(dir_path, "anonymized_logs", "amun")
     amun_dir=os.path.join(dir_path,"anonymized_logs","amun_sub")
-    pripel_trace_dir = os.path.join(dir_path, "anonymized_logs", "pripel","trace_variant")
-    pripel_time_dir = os.path.join(dir_path, "anonymized_logs", "pripel","time")
+    # pripel_trace_dir = os.path.join(dir_path, "anonymized_logs", "pripel","trace_variant")
+    # pripel_time_dir = os.path.join(dir_path, "anonymized_logs", "pripel","time")
+    pripel_trace_dir = os.path.join(dir_path, "anonymized_logs", "pripel")
+    pripel_time_dir = os.path.join(dir_path, "anonymized_logs", "pripel")
+    libra_dir=os.path.join(dir_path, "anonymized_logs", "libra")
 
     for dataset in datasets:
         org_path=os.path.join(dir_path,"data",dataset+".xes")
         files=list(os.walk(amun_dir))[0][2]
         for log in files:
             if log.find(dataset)!=-1:
-                anonymized_dir=os.path.join(amun_dir,log)
-                generate_jobs("emd", org_path, anonymized_dir, comparison_dir, dataset, "amun", log)
+                """Amun"""
+                # anonymized_dir=os.path.join(amun_dir,log)
+                # generate_jobs("emd", org_path, anonymized_dir, comparison_dir, dataset, "amun", log)
                 # generate_jobs("jaccard", org_path, anonymized_dir, comparison_dir, dataset, "amun", log)
                 # compare_emd(org_path,anonymized_dir,comparison_dir)
                 # compare_jaccard(org_path, anonymized_dir, comparison_dir)
-        #
-        # files = list(os.walk(pripel_trace_dir))[0][2]
-        # for log in files:
-        #     if log.find(dataset)!=-1:
-        #         anonymized_dir = os.path.join(pripel_trace_dir, log)
-        #         generate_jobs("jaccard", org_path, anonymized_dir, comparison_dir, dataset, "pripel", log)
-        #         # compare_jaccard(org_path, anonymized_dir, comparison_dir)
 
-        # files = list(os.walk(pripel_time_dir))[0][2]
-        # for log in files:
-        #     if log.find(dataset)!=-1:
-        #         anonymized_dir = os.path.join(pripel_time_dir, log)
-        #         generate_jobs("emd", org_path, anonymized_dir, comparison_dir, dataset, "pripel", log)
-        #         # compare_emd(org_path, anonymized_dir, comparison_dir)
+                """Pripel"""
+                files = list(os.walk(pripel_trace_dir))[0][2]
+                for log in files:
+                    if log.find(dataset)!=-1:
+                        anonymized_dir = os.path.join(pripel_trace_dir, log)
+                        generate_jobs("jaccard", org_path, anonymized_dir, comparison_dir, dataset, "pripel", log)
+                        # compare_jaccard(org_path, anonymized_dir, comparison_dir)
+
+                """SaCoFa"""
+                # files = list(os.walk(pripel_time_dir))[0][2]
+                # for log in files:
+                #     if log.find(dataset)!=-1:
+                #         anonymized_dir = os.path.join(pripel_time_dir, log)
+                #         generate_jobs("emd", org_path, anonymized_dir, comparison_dir, dataset, "pripel", log)
+                #         # compare_emd(org_path, anonymized_dir, comparison_dir)
+
+                """Libra"""
+                # anonymized_dir = os.path.join(libra_dir, log)
+                # generate_jobs("emd", org_path, anonymized_dir, comparison_dir, dataset, "libra", log)
+                # generate_jobs("jaccard", org_path, anonymized_dir, comparison_dir, dataset, "libra", log)
