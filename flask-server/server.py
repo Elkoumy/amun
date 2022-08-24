db749c3aca06accb978bd2a1c077d9238b172a20
elkoumy <gamal.elkomy@gmail.com>
2022-08-24 15:26:31 +0300

    fix the combo docker file Amun.docker

--- flask-server/server.py
+++ flask-server/server.py
@@ -23,7 +23,7 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route('/')
 def index():
-    return app.send_static_file('../build/index.html')
+    return app.send_static_file('/index.html')
 
 
 
