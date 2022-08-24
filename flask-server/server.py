ace8b31bfb437ef044202edb8a7340fd15808be6
elkoumy <gamal.elkomy@gmail.com>
2022-08-24 15:39:50 +0300

    fix service name conflict with page name

--- flask-server/server.py
+++ flask-server/server.py
@@ -51,7 +51,7 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route('/upload', methods=['GET', 'POST'])
+@app.route('/uploadLog', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
         # check if the post request has the file part
