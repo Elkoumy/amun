1ebbe730ca0ac5102b76e6460efd98dea786fff5
elkoumy <gamal.elkomy@gmail.com>
2022-11-13 19:57:51 +0200

    run emd for Pripel

--- main_comparison_event_log.py
+++ main_comparison_event_log.py
@@ -51,13 +51,11 @@ if __name__ == "__main__":
     #             "BPIC20_t",
     #             "BPIC12_t", "BPIC13_t", "BPIC15_t", "BPIC17_t", "BPIC18_t", "BPIC19_t"]
 
-    datasets = ["Traffic_t", "Hospital_t", "CreditReq_t",
+
+
+    datasets = ["CCC19_t", "Unrineweginfectie_t", "BPIC14_t", "Traffic_t", "Hospital_t", "CreditReq_t",
                 "BPIC20_t",
                 "BPIC12_t", "BPIC13_t", "BPIC15_t", "BPIC17_t", "BPIC18_t", "BPIC19_t"]
-
-    # datasets = ["CCC19_t", "Unrineweginfectie_t", "BPIC14_t", "Traffic_t", "Hospital_t", "CreditReq_t",
-    #             "BPIC20_t",
-    #             "BPIC12_t", "BPIC13_t", "BPIC15_t", "BPIC17_t", "BPIC18_t", "BPIC19_t"]
     # datasets=["Sepsis_t","BPIC20_t","BPIC13_t"]
     dir_path = os.path.dirname(os.path.realpath(__file__))
     comparison_dir = os.path.join(dir_path, "comparison")
