597d5984bbc329dd2133b863c3c39f6a8f674f08
elkoumy <gamal.elkomy@gmail.com>
2022-11-13 19:53:38 +0200

    run emd for Pripel

--- main_comparison_event_log.py
+++ main_comparison_event_log.py
@@ -83,23 +83,23 @@ if __name__ == "__main__":
         #         # compare_jaccard(org_path, anonymized_dir, comparison_dir)
 
         # """Pripel"""
-        # files = list(os.walk(pripel_trace_dir))[0][2]
-        #
-        # for log in files:
-        #     if log.find(dataset)!=-1:
-        #         anonymized_dir = os.path.join(pripel_trace_dir, log)
-        #         # generate_jobs("jaccard", org_path, anonymized_dir, comparison_dir, dataset, "pripel", log)
-        #         generate_jobs("emd", org_path, anonymized_dir, comparison_dir, dataset, "pripel", log)
-        #         # compare_jaccard(org_path, anonymized_dir, comparison_dir)
+        files = list(os.walk(pripel_trace_dir))[0][2]
 
-        """SaCoFa"""
-        files = list(os.walk(sacofa_dir))[0][2]
         for log in files:
             if log.find(dataset)!=-1:
-                anonymized_dir = os.path.join(sacofa_dir, log)
-                # generate_jobs("jaccard", org_path, anonymized_dir, comparison_dir, dataset, "sacofa", log)
-                generate_jobs("emd", org_path, anonymized_dir, comparison_dir, dataset, "sacofa", log)
-                # compare_emd(org_path, anonymized_dir, comparison_dir)
+                anonymized_dir = os.path.join(pripel_trace_dir, log)
+                # generate_jobs("jaccard", org_path, anonymized_dir, comparison_dir, dataset, "pripel", log)
+                generate_jobs("emd", org_path, anonymized_dir, comparison_dir, dataset, "pripel", log)
+                # compare_jaccard(org_path, anonymized_dir, comparison_dir)
+
+        # """SaCoFa"""
+        # files = list(os.walk(sacofa_dir))[0][2]
+        # for log in files:
+        #     if log.find(dataset)!=-1:
+        #         anonymized_dir = os.path.join(sacofa_dir, log)
+        #         # generate_jobs("jaccard", org_path, anonymized_dir, comparison_dir, dataset, "sacofa", log)
+        #         generate_jobs("emd", org_path, anonymized_dir, comparison_dir, dataset, "sacofa", log)
+        #         # compare_emd(org_path, anonymized_dir, comparison_dir)
 
         # """Libra"""
         # files = list(os.walk(libra_dir))[0][2]
