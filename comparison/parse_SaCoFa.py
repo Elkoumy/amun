b012c5db065383525e80fb8cf544671cdfd236d7
elkoumy <gamal.elkomy@gmail.com>
2022-11-14 19:06:16 +0200

    parse results

--- comparison/parse_SaCoFa.py
+++ comparison/parse_SaCoFa.py
@@ -7,12 +7,13 @@ def preprocess_jaccard(row):
     row['engine'] = 'SaCoFa'
     row['dataset'] = row['file'].split("_")[0]
     row['epsilon']=row['file'].split("_")[2]
-    if row['file'].split("_")[2]=='0.81':
-        row['delta'] = '0.2'
-    elif row['file'].split("_")[2]=='1.238':
-        row['delta'] = '0.3'
-    elif row['file'].split("_")[2]=='1.7':
-        row['delta'] = '0.4'
+    row['delta'] =  row['file'].split("_")[7]
+    # if row['file'].split("_")[2]=='0.81':
+    #     row['delta'] = '0.2'
+    # elif row['file'].split("_")[2]=='1.238':
+    #     row['delta'] = '0.3'
+    # elif row['file'].split("_")[2]=='1.7':
+    #     row['delta'] = '0.4'
     row['N']=row['file'].split("_")[5][1:]
     row['P']=row['file'].split("_")[6][1:]
     row['prunning']=row['file'].split("_")[3]+'_'+row['file'].split("_")[4]
@@ -20,8 +21,8 @@ def preprocess_jaccard(row):
     return row
 
 
-data=pd.read_csv(r"C:\Gamal Elkoumy\PhD\OneDrive - Tartu Ülikool\Differential Privacy\amun\comparison\SaCoFa_results\SaCoFa_Sepsis.csv")
+data=pd.read_csv(r"C:\Users\elkoumy\OneDrive - Tartu Ülikool\Differential Privacy\amun\comparison\emd_all_sacofa.txt")
 data=data.apply(preprocess_jaccard,axis=1)
 data=data.drop('file',axis=1)
-data.to_csv(r"C:\Gamal Elkoumy\PhD\OneDrive - Tartu Ülikool\Differential Privacy\amun\comparison\SaCoFa_results\SaCoFa_Sepsis_parsed.csv",index=False)
+data.to_csv(r"C:\Users\elkoumy\OneDrive - Tartu Ülikool\Differential Privacy\amun\comparison\emd_all_sacofa_parsed.csv",index=False)
 print("**")
